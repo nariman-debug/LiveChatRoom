cca579e688021b82eadff36999146429408f144f
nariman-debug <cryptocurrency.a1765@gmail.com>
2025-07-03 23:09:12 +0330

    craete base live chat room project

---
+++ LiveChatRoom/asgi.py
@@ -0,0 +1,25 @@
+"""
+ASGI config for LiveChatRoom project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chatroom.routing
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LiveChatRoom.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chatroom.routing.websocket_urlpatterns
+        )
+    ),
+})
